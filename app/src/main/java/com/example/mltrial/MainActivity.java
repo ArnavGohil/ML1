1f3a581281d5e6d26048f47cea3a7807f7a0f23d
Arnav <arnav.gohil04@gmail.com>
2020-02-16 12:36:55 +0530

    :tada: commit

---
+++ app/src/main/java/com/example/mltrial/MainActivity.java
@@ -0,0 +1,147 @@
+package com.example.mltrial;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.common.FirebaseMLException;
+import com.google.firebase.ml.common.modeldownload.FirebaseLocalModel;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelManager;
+import com.google.firebase.ml.common.modeldownload.FirebaseRemoteModel;
+import com.google.firebase.ml.custom.FirebaseModelDataType;
+import com.google.firebase.ml.custom.FirebaseModelInputOutputOptions;
+import com.google.firebase.ml.custom.FirebaseModelInputs;
+import com.google.firebase.ml.custom.FirebaseModelInterpreter;
+import com.google.firebase.ml.custom.FirebaseModelOptions;
+import com.google.firebase.ml.custom.FirebaseModelOutputs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView out ;
+    EditText in ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        in = findViewById(R.id.input);
+        out = findViewById(R.id.output);
+
+        FirebaseModelDownloadConditions.Builder conditionsBuilder =
+                new FirebaseModelDownloadConditions.Builder().requireWifi();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            // Enable advanced conditions on Android Nougat and newer.
+            conditionsBuilder = conditionsBuilder
+                    .requireCharging()
+                    .requireDeviceIdle();
+        }
+        FirebaseModelDownloadConditions conditions = conditionsBuilder.build();
+
+        // Build a remote model source object by specifying the name you assigned the model
+        // when you uploaded it in the Firebase console.
+        FirebaseRemoteModel cloudSource = new FirebaseRemoteModel.Builder("c2f")
+                .enableModelUpdates(true)
+                .setInitialDownloadConditions(conditions)
+                .setUpdatesDownloadConditions(conditions)
+                .build();
+        FirebaseModelManager.getInstance().registerRemoteModel(cloudSource);
+
+        FirebaseLocalModel localSource =
+                new FirebaseLocalModel.Builder("my_local_model")  // Assign a name to this model
+                        .setAssetFilePath("model.tflite")
+                        .build();
+        FirebaseModelManager.getInstance().registerLocalModel(localSource);
+
+    }
+
+
+    public void Click(View view)
+    {
+        float val = Float.parseFloat(in.getText().toString());
+        try {
+            runInference(val);
+        } catch (FirebaseMLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    
+    private FirebaseModelInterpreter createInterpreter() throws FirebaseMLException {
+        // [START mlkit_create_interpreter]
+        FirebaseModelOptions options = new FirebaseModelOptions.Builder()
+                .setRemoteModelName("c2f")
+                .setLocalModelName("my_local_model")
+                .build();
+        FirebaseModelInterpreter firebaseInterpreter =
+                FirebaseModelInterpreter.getInstance(options);
+        // [END mlkit_create_interpreter]
+
+        return firebaseInterpreter;
+    }
+
+    private FirebaseModelInputOutputOptions createInputOutputOptions() throws FirebaseMLException {
+        // [START mlkit_create_io_options]
+        FirebaseModelInputOutputOptions inputOutputOptions =
+                new FirebaseModelInputOutputOptions.Builder()
+                        .setInputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1,1})
+                        .setOutputFormat(0, FirebaseModelDataType.FLOAT32, new int[]{1, 1})
+                        .build();
+        // [END mlkit_create_io_options]
+
+        return inputOutputOptions;
+    }
+
+
+    private void runInference(float n) throws FirebaseMLException {
+        FirebaseModelInterpreter firebaseInterpreter = createInterpreter();
+        float[][] input = new float[1][1];
+        input[0][0] = n ;
+        FirebaseModelInputOutputOptions inputOutputOptions = createInputOutputOptions();
+
+        // [START mlkit_run_inference]
+        FirebaseModelInputs inputs = new FirebaseModelInputs.Builder()
+                .add(input)  // add() as many input arrays as your model requires
+                .build();
+        firebaseInterpreter.run(inputs, inputOutputOptions)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseModelOutputs>() {
+                            @Override
+                            public void onSuccess(FirebaseModelOutputs result) {
+                                // [START_EXCLUDE]
+                                // [START mlkit_read_result]
+                                float[][] output = result.getOutput(0);
+                                out.setText(output[0][0] + " ");
+                                // [END mlkit_read_result]
+                                // [END_EXCLUDE]
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                // ...
+                            }
+                        });
+        // [END mlkit_run_inference]
+    }
+}
